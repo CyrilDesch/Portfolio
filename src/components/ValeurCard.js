d28d2e45290ce12a2a84bf98d4f430e12337d590
Cyril Deschamps <77438408+CyrilDlaPompote@users.noreply.github.com>
2021-06-30 21:42:44 +0200

    v0.1 page accueil

--- src/components/ValeurCard.js
+++ src/components/ValeurCard.js
@@ -3,52 +3,18 @@ import IconDecouverte from '../assets/IconDecouverte';
 import IconPassion from '../assets/IconPassion';
 import IconSucces from '../assets/IconSucces';
 
-const ValeurCard = ({ title, paragraphe, presetIcon, childNb }) => {
-
-  const styleChoice = (type) => {
-    if (type === "icon")
-      return {...(childNb === 1 || childNb === 3 ? styles.droit : styles.gauche)};
-    else if (type === "card")
-      return {...(childNb === 1 || childNb === 3 ? styles.borderRight : styles.borderLeft), ...(childNb === 1 || childNb === 2 || childNb === 3 ? styles.borderBottom : null)};
-  }
+const ValeurCard = ({ title, paragraphe, presetIcon }) => {
 
   return(
-    <div className="valeurCard" style={styleChoice("card")}>
-      <div className="valeurCardContainer">
-        <div className="iconContainer" style={styleChoice("icon")} >
-          {presetIcon === "succes" ? <IconSucces className="iconSucces" /> : null}
-          {presetIcon === "passion" ? <IconPassion className="iconSucces" /> : null}
-          {presetIcon === "decouverte" ? <IconDecouverte className="iconSucces" /> : null}
-          {presetIcon === "communication" ? <IconCommunication className="iconSucces" /> : null}
-        </div>
-        <p className="title">{title}</p>
-        <p className="paragraphe">{paragraphe}</p>
-      </div>
+    <div className="valeurCard" >
+      {presetIcon === "succes" ? <IconSucces className="icon" /> : null}
+      {presetIcon === "passion" ? <IconPassion className="icon" /> : null}
+      {presetIcon === "decouverte" ? <IconDecouverte className="icon" /> : null}
+      {presetIcon === "communication" ? <IconCommunication className="icon" /> : null} 
+      <h1>{title}</h1>
+      <p>{paragraphe}</p>
     </div>
   );
-}
-
-const styles = {
-  droit: {
-    left: "-10vw"
-  },
-  gauche: {
-    right: "-10vw"
-  },
-  borderLeft: {
-    borderLeft: "solid 0.15vw var(--appcolor)",
-    padding: "4vw 20vw 4vw 15vw",
-  },
-  borderRight: {
-    borderRight: "solid 0.15vw var(--appcolor)",
-    padding: "4vw 15vw 4vw 20vw"
-  },
-  borderTop: {
-    borderTop: "solid 0.15vw var(--appcolor)",
-  },
-  borderBottom: {
-    borderBottom: "solid 0.15vw var(--appcolor)",
-  },
 }
 
 export default ValeurCard;
--- src/pages/App.js
+++ src/pages/App.js
@@ -6,6 +6,18 @@ import { SocialIcon } from 'react-social-icons';
 import useScroll from "../hooks/useScroll";
 import UpArrow from "../assets/UpArrow";
 import PlayButton from "../assets/PlayButton";
+import Competence from "../components/Competence";
+import ValeurCard from '../components/ValeurCard';
+
+//IMPORT ICON
+import AndroidIcon from '../assets/iconExt/android.svg';
+import CssHtmlIcon from '../assets/iconExt/css_html.svg';
+import FirebaseIcon from '../assets/iconExt/firebase.svg';
+import GitHubIcon from '../assets/iconExt/github.svg';
+import JavascriptIcon from '../assets/iconExt/javascript.svg';
+import NodeJSIcon from '../assets/iconExt/nodeJS.svg';
+import ReactIcon from '../assets/iconExt/react.svg';
+import TrelloIcon from '../assets/iconExt/trello.svg'
 
 const App = () => {
   const scrolled = useScroll();
@@ -132,6 +144,9 @@ const App = () => {
           <UpArrow className="iconUp" onClick={() => animateScroll.scrollToTop()} />
         : null}
         <img className="postItBG" src="./assets/postit.png" alt="Post it BG" />
+        <img className="postItBG" src="./assets/postit.png" alt="Post it BG" />
+        <img className="postItBG" src="./assets/postit.png" alt="Post it BG" />
+        <img className="postItBG" src="./assets/postit.png" alt="Post it BG" />
         <div>
           <div>
             <div className="firstContainer">
@@ -170,16 +185,79 @@ const App = () => {
               </div>
             </div>
             <div className="secondContainer">
-              <h1>PRESENTATION</h1>
+              <h1>PRÉSENTATION</h1>
               <h2>Qui suis-je ?</h2>
               <div>
-                <h1></h1>
+                <h1>Passionné par l'apprentissage et par la création</h1>
+                <p>Je m'appelle Cyril Deschamps. Ayant toujours été autodidacte, très débrouillard et d’une grande ouverture d’esprit, j’ai acquis de nombreuses connaissances dans divers domaines et notamment dans le développement informatique et la création de projet. Par cet apprentissage, la programmation et l'entrepreneuriat sont devenues mes principales passions. De plus, mes études d’informatiques m’ont beaucoup apporté dans ces deux passions. <strong>Aujourd’hui, mon rôle est d’accompagner les entreprises dans la réalisation et la maintenance de leur application mobile pour mener à bien leurs projets.</strong></p>
+              </div>
+            </div>
+            <div className="thirdContainer">
+              <svg className="intersection" viewBox="0 0 200 300">
+                <line      
+                  x1="100" x2="100"
+                  y1="40" y2="260"
+                  stroke="#cdcdcd" 
+                  strokeWidth="0.5vw"
+                  strokeLinecap="round"
+                  strokeDasharray="9, 16"
+                />
+              </svg>    
+              <h1>COMPÉTENCES</h1>
+              <h2>TECHNIQUES</h2>
+              <div className="competencesContainer">
+                <Competence icon={ReactIcon} title="React Native" desc={["○ Maitrise complète", "○ Tout type de projet"]} />
+                <Competence icon={JavascriptIcon} title="Javascript" desc={["○ Maitrise complète pour React Native"]} />
+                <Competence icon={AndroidIcon} title="Android/Java" desc={["○ Niveau intermédiaire"]} />
+                <Competence icon={CssHtmlIcon} title="HTML/CSS" desc={["○ Maitrise complète"]} />
+                <Competence icon={FirebaseIcon} title="Firebase" desc={["○ Bonne connaissance des outils", "○ Utilisation de Firestore"]} />
+                <Competence icon={NodeJSIcon} title="NodeJS" desc={["○ Niveau intermédiaire", "○ Capable de créer un back-end"]} />
+                <Competence icon={GitHubIcon} title="GitHub (versioning)" desc={["○ Maitrise complète", "○ Indispensable"]} />
+                <Competence icon={TrelloIcon} title="Trello (planification)" desc={["○ Maitrise complète", "○ Utilisation quotidienne"]} />
+              </div>
+            </div>
+            <div className="fourthContainer">
+              <svg className="intersection" viewBox="0 0 200 300">
+                <line      
+                  x1="100" x2="100"
+                  y1="40" y2="260"
+                  stroke="#cdcdcd" 
+                  strokeWidth="0.5vw"
+                  strokeLinecap="round"
+                  strokeDasharray="9, 16"
+                />
+              </svg>    
+              <h1>MES VALEURS</h1>
+              <div className="valeursContainer">
+                <ValeurCard 
+                  presetIcon="succes"
+                  title="Mon optimisme, mon ambition et mon goût de la réussite" 
+                  paragraphe="Peu importe votre idée et la quantité de travail demandée, c’est avec toute ma bonne volonté que je vous aiderai et vous conseillerai pour mener à bien vos projets."  
+                />
+                <ValeurCard 
+                  title="Je suis passionné" 
+                  paragraphe="La création informatique est ma passion. Le fait de pouvoir créer n'importe quel projet, seul ou à plusieurs, m’a permis de m’épanouir." 
+                  presetIcon="passion"
+                />
+                <ValeurCard 
+                  presetIcon="decouverte"
+                  title="L'amour de la découverte" 
+                  paragraphe="Ayant toujours été très débrouillard, j’ai appris de nombreuses choses seul et je suis finalement autodidacte. Je ressens le besoin de comprendre tout ce qui m’entoure pour évoluer."
+                />
+                <ValeurCard 
+                  presetIcon="communication" 
+                  title="Ne pas se précipiter !" 
+                  paragraphe="Je donne beaucoup d'importance à la communication dans un projet, surtout entre les différentes branches de métiers. Je ferai mon maximum pour cerner totalement le besoin de votre application et répondre à vos demandes." 
+                />
               </div>
             </div>
           </div>
           <div />
         </div>
       </main>
+      <footer>
+        
+      </footer>
     </>
   );
 }
